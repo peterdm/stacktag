2c8be1c3e621532124f72f1cd5c046bd7fe14155
Peter Dixon-Moses <peter@searchintuition.com>
2012-05-25 19:12:20 -0400

    working for multi-question pages.  needed a setTimeout to work around ajax load for now.

--- suggest.js
+++ suggest.js
@@ -2,63 +2,86 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-var tags = [];
-var suggestions = {};
 
-function getTags() {
-	var query = "";
 
-	$.each($('.post-taglist .post-tag'), function(index, value) {
-		console.log(value.text);
-		tags.push(value.text);
-		query += value.text + " ";
-	})
-	
-	return query;
+function suggestTags(parentElement, childElements) {
+  var query = "";
+  var tags = [];
+
+  $.each(childElements, function(index, value) {
+	console.log(value.text);
+	tags.push(value.text);
+	query += value.text + " ";
+  });
+
+  if (query) {
+  	$.getJSON('http://localhost:8983/solr/posts/tagsuggest', { q: query }, createCallback(parentElement, tags));
+  }
+}
+
+
+function layoutSuggestions(data, parentElement, tags) {
+    var suggestions = {};
+	var lasttag = "";
+	var numberFound = data['response']['numFound'];
+
+	$.each(data['facet_counts']['facet_fields']['QuestionTags'], function(index, value) {
+		if (index%2==0) {
+			lasttag = value;
+		} else {
+			if (!exists(tags, lasttag)) // new suggestions
+			suggestions[lasttag] = value;
+		}
+	});
+
+	var link = '';
+	$.each(suggestions, function(key, value) {
+		link = $('<a/>').addClass('post-tag');
+		link.text(key);
+		link.attr('href', '/questions/tagged/'+key);
+		link.attr('rel', 'tag');
+		var opacity = 1 / Math.log(1/(value/numberFound)); // turn exp dropoff into geo
+		link.css({
+			'color' : '#7b9fb8',
+			'background-color' : 'rgba(224, 234, 241,' + opacity + ')',
+			'border-bottom' : '1px solid rgba(62, 109, 142,' + opacity + ')',
+			'border-right' : '1px solid rgba(127, 159, 182,' + opacity + ')'
+		});
+		parentElement.append(link);
+		console.log(parentElement.parent().parent().attr('id') + ': ' + key + ' (' + value + ')');
+	});
+
 };
 
-function searchTags(query, callback) {
-  $.getJSON('http://localhost:8983/solr/posts/tagsuggest', { q: query }, callback);	
+
+function createCallback(parentElement, tags) {
+   return function(data) {
+      layoutSuggestions(data, parentElement, tags);
+   };
 };
+ 
+
 
 function exists(arr,obj) {
     return (arr.indexOf(obj) != -1);
 };
 
-function onResults(data) {
-  var lasttag = "";
-  var numberFound = data['response']['numFound'];
-
-  $.each(data['facet_counts']['facet_fields']['QuestionTags'], function(index, value) {
-    if (index%2==0) {
-	    lasttag = value;
-    } else {
-	    if (!exists(tags, lasttag)) // new suggestions
-			suggestions[lasttag] = value;
-	}
-  });
-
-  $.each(suggestions, function(key, value) {
-    var link = $('<a/>').addClass('post-tag');
-    link.text(key);
-	link.attr('href', '/questions/tagged/'+key);
-	link.attr('rel', 'tag');
-	var opacity = 1 / Math.log(1/(value/numberFound)); // turn exp dropoff into geo
-	link.css({
-		'color' : '#7b9fb8',
-		'background-color' : 'rgba(224, 234, 241,' + opacity + ')',
-		'border-bottom' : '1px solid rgba(62, 109, 142,' + opacity + ')',
-		'border-right' : '1px solid rgba(127, 159, 182,' + opacity + ')'
-	});
-	$('.post-taglist').first().append(link);
-	console.log(key + ' (' + value + ')');
-  });
-
-};
 
 
-$(document).ready(function () {
-	searchTags(getTags(), onResults);
+
+$(window).load(function () {
+	// handle question detail page
+	suggestTags($('.post-taglist'), $('.post-taglist').find('.post-tag') );
+
+	setTimeout(function() {		
+		// handle question summary page (delay for ajax load -- caching needs a cmd-reload to demo)
+		$.each($('.tags'), function(index, value) {
+		 	parentElement = $(value);
+			suggestTags(parentElement, parentElement.find('.post-tag') );	
+			return;
+		});
+	}, 500);
+	
 });
 
 
